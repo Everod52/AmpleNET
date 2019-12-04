bcfff395f710e9dfe754d4d6c04a3e90a4498184
Everson Rodriguez <everson.rodriguez@upr.edu>
2019-12-04 01:30:42 -0400

    Refactored lexer

--- lexer.py
+++ lexer.py
@@ -1,6 +1,7 @@
 import ply.lex as lex
 
 
+# Reserved words
 reserved_words = {
     'connect': 'CONNECT',
     'open': 'OPEN',
@@ -18,18 +19,18 @@ tokens = [
 ] + list(reserved_words.values())
 
 
-# REGULAR EXPRESSIONS FOR TOKENS
+# REGULAR EXPRESSION RULES
+# Left bracket
 t_LB = r'\['
+
+# Right bracket
 t_RB = r'\]'
+
+# Exclamation point
 t_EXCLAMATION = r'!'
+
+# Semicolon
 t_SEMICOLON = r';'
-
-t_ignore = '\n \t'
-
-
-def t_error(t):
-    print('Error Illegal character')
-    t.lexer.skip(1)
 
 
 # Match numbers
@@ -47,7 +48,26 @@ def t_ID(t):
     return t
 
 
+# Characters to ignore
+t_ignore = '\n \t'
+
+
+# Error rule
+def t_error(t):
+    print('Error Illegal character')
+    t.lexer.skip(1)
+
+
+# # Comment
+# def t_COMMENTS(t):
+#     r'\%.*'
+#     pass
+
+
+# Build the lexer
 lexer = lex.lex()
+
+# Read the input
 lexer.input('connect[1;2]!'
             'open[s2]'
             'send[serverMessage]')
